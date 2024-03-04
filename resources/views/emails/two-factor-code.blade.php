d78a14e21ac91882fc892f89a81c9bc8e0c97432
Aaron Florey <azza@jcaks.net>
2024-03-05 10:49:12 +1100

    fix: remove edocs from the email template

--- resources/views/emails/two-factor-code.blade.php
+++ resources/views/emails/two-factor-code.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
     Your two-factor authentication code: <strong>{{ $code }}</strong>
 
-    Use this code to complete logging in to eDocs.
+    Use this code to complete logging in.
 @endcomponent
